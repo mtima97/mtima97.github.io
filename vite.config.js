513cb461e9eb2eaf64e683a0822eefe7bf8c78b9
Timur Myngbay <mtima97@gmail.com>
2024-09-19 00:41:50 +0500

    add pckgs

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+// noinspection JSUnusedGlobalSymbols
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,12 +7,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })

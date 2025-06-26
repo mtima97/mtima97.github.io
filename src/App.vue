1272a7234e3dee1680cbc37f7c3ac1e6c9f809c5
Timur Myngbay <mtima97@gmail.com>
2025-06-26 14:13:55 +0500

    add scss support

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
-<script setup>
-import {ref, watch, computed} from 'vue'
-
-const msg = 'Hello World!';
-const wifeName = 'Fari'
-let fullWifeName = computed(() => wifeName.concat('za'));
-let count = ref(0)
-
-watch(count, (nv, ov) => console.log(nv, ov))
-
-function greet(name) {
-    alert(`Hello ${name}!, your wife's name is ${wifeName}`);
-}
-</script>
-
 <template>
-    <header>
-        <h1 :title="msg">Hello My Friends</h1>
-        <button :title="fullWifeName" @click.prevent="greet('Timur')">Click</button>
-    </header>
+    <div></div>
 </template>
 
-<style scoped>
-button {
-    border: none;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import 'normalize.css'
-import '@/assets/main.css'
+import './assets/main.scss'
 
 import { createApp } from 'vue'
 import App from '@/App.vue'

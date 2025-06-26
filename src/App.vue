84837e091f47068ce162f07c1f58b32b54bbc2a1
Timur Myngbay <mtima97@gmail.com>
2025-06-26 14:41:03 +0500

    update deps and start markup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
-    <div></div>
+    <div class="cv-container">
+        <aside class="cv-sidebar"></aside>
+        <main class="cv-main"></main>
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cv-container {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    max-width: 1000px;
+    margin: 0 auto;
+    min-height: 100vh;
+    font-family: sans-serif;
+    background-color: white;
+}
+
+.cv-sidebar {
+    background-color: #f5f5f5;
+    padding: 20px;
+}
+
+.cv-main {
+    padding: 20px;
+}
+</style>

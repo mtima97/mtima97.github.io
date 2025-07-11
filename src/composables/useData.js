d85cae1ff98d7ba46a359beef75e8f7e5565f224
Timur Myngbay <mtima97@gmail.com>
2025-07-11 16:53:13 +0500

    add en

--- src/composables/useData.js
+++ src/composables/useData.js
@@ -1,11 +1,22 @@
 import { computed } from 'vue'
+
+import { useLanguage } from './useLanguage.js'
+
 import ruData from '@/data/ru/data.json'
 import ruLongTexts from '@/data/ru/long_texts.js'
+import enData from '@/data/en/data.json'
+import enLongTexts from '@/data/en/long_texts.js'
 
-const ru = Object.assign({texts: ruLongTexts}, ruData)
+const ru = {...ruData, texts: ruLongTexts}
+
+const en = {...enData, texts: enLongTexts}
 
 export function useData() {
+    const { isRu } = useLanguage()
+
+    const data = computed(() => isRu.value ? ru : en)
+
     return {
-        data: computed(() => ru)
+        data,
     }
 }
---
+++ src/data/en/long_texts.js
@@ -0,0 +1,14 @@
+const summary = `Hello! My name is Tima, a Go developer at Wildberries.
+My job is to build high-performance backend systems with a strong focus on code organization and architectural clarity.
+In my daily work, I use <i>Go</i>, <i>PostgreSQL</i>, <i>Kafka</i> и <i>Docker</i>.`
+
+const summaryVue = `I have experience with <span class="w3-text-teal">Vue</span> and am
+ready to take on frontend tasks when needed.`
+
+const runText = `In my free time, I go running <span class="w3-text-teal w3-small">(Strava)</span> and read.`
+
+export default {
+    summary,
+    summaryVue,
+    runText
+}

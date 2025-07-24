3abbe75bd17f8da23a27aa69ecda52cd3e3b76d0
Timur Myngbay <mtima97@gmail.com>
2025-07-24 23:13:13 +0500

    remove decoration

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -5,7 +5,7 @@
         </h5>
         <p v-for="d in project.descriptions">
             <span>{{ `${d.text}&nbsp;` }}</span>
-            <a v-if="d.link" :href="d.link" target="_blank" class="w3-tag w3-teal w3-round w3-small w3-hover-shadow">
+            <a v-if="d.link" :href="d.link" target="_blank" :class="linkClasses">
                 {{ d.linkText }}
             </a>
         </p>
@@ -18,4 +18,12 @@ defineProps({
     project: Object,
     isHidden: Boolean,
 })
+
+const linkClasses = 'w3-tag w3-teal w3-round w3-small w3-hover-shadow'
 </script>
+
+<style lang="scss" scoped>
+a {
+	text-decoration: none;
+}
+</style>

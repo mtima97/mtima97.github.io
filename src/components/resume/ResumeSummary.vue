cb4226e44fa55f851b3db07c43a0a8ad120ff233
Timur Personal <mtima97@gmail.com>
2026-02-13 16:41:44 +0500

    rf: add summary title

--- src/components/resume/ResumeSummary.vue
+++ src/components/resume/ResumeSummary.vue
@@ -2,9 +2,14 @@
 import { useCvStore } from '@/stores/cv_v2.js'
 import { useLanguageStore } from '@/stores/language_v2.js'
 import { get } from 'lodash'
+import { computed } from 'vue'
 
 const cvStore = useCvStore()
 const languageStore = useLanguageStore()
+
+const title = computed(() => {
+    return languageStore.isRu ? 'Желаемая зарплата' : 'Expected salary'
+})
 </script>
 
 <template>
@@ -15,7 +20,7 @@ const languageStore = useLanguageStore()
 
         <p class="w3-small">{{ get(cvStore.cv, 'profile.summary') }}</p>
 
-        <p class="w3-small w3-text-grey">
+        <p class="w3-small w3-text-grey" :title="title">
             {{ cvStore.finance.salary }} {{ cvStore.finance.salary_sfx }}
         </p>
     </section>
